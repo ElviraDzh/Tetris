270cb539393145c7ee0ac648f66dd62258f63c7e
Elvira Dzhumalieva <elvira@Elviras-MacBook-Pro.local>
2022-05-23 16:19:45 +0100

    Initial changes in git.

--- 06:math-game/app.js
+++ 06:math-game/app.js
@@ -1,5 +1,4 @@
 let users = JSON.parse(window.localStorage.getItem("users"));
-console.log(users);
 if (users === null) {
   users = {};
 }
@@ -7,15 +6,6 @@ for (let user in users) {
   createDropdown(user, users[user]);
   window.localStorage.setItem("users", JSON.stringify(users));
 }
-console.log(users);
-// const users = {};
-// const savedUsers = JSON.parse(window.localStorage.getItem("users"));
-// console.log(savedUsers);
-
-// for (let savedUser in savedUsers) {
-//   createDropdown(savedUser, savedUsers[savedUser]);
-//   window.localStorage.setItem("users", JSON.stringify(savedUsers));
-// }
 
 function addNewUser() {
   const userName = document.getElementById("userName").value;
@@ -26,19 +16,16 @@ function addNewUser() {
 }
 
 function createDropdown(userName, scr) {
-  const dropdown = createNewElement("div", "dropdown");
-  dropdown.setAttribute("id", userName);
+  const dropdown = createNewElement("div", "dropdown", userName);
 
   const mainBlock = document.querySelector(".main-block");
   mainBlock.append(dropdown);
 
-  const dropbtn = createNewElement("button", "btn-dropdown");
-  dropbtn.setAttribute("id", `${userName}Btn`);
+  const dropbtn = createNewElement("button", "btn-dropdown", `${userName}Btn`);
   dropbtn.innerText = userName + " (";
 
-  const score = createNewElement("span", "score");
+  const score = createNewElement("span", "score", `${userName}Score`);
   score.innerText = scr;
-  score.setAttribute("id", `${userName}Score`);
   dropbtn.appendChild(score);
 
   const bracket = createNewElement("span", "bracket");
@@ -51,29 +38,28 @@ function createDropdown(userName, scr) {
 
   dropdown.appendChild(dropbtn);
 
-  const dropdownContent = createNewElement("div", "dropdown-content");
-  dropdownContent.setAttribute("id", `${userName}DCont`);
+  const dropdownContent = createNewElement(
+    "div",
+    "dropdown-content",
+    `${userName}DCont`
+  );
   dropdown.append(dropdownContent);
 
-  const play = createNewElement("p", "content");
-  play.setAttribute("id", `${userName}Play`);
+  const play = createNewElement("a", "content", `${userName}Play`);
   play.innerText = "play";
   play.addEventListener("click", () => {
     checkMath(userName);
   });
 
-  const del = createNewElement("p", "content");
-  del.setAttribute("id", `${userName}Del`);
+  const del = createNewElement("a", "content", `${userName}Del`);
   del.innerText = "delete";
   del.addEventListener("click", () => {
     dropdown.remove();
     delete users[userName];
     localStorage.setItem("users", JSON.stringify(users));
-    console.log(users); // prints { "objtwo": two}
   });
 
-  const reset = createNewElement("p", "content");
-  reset.setAttribute("id", `${userName}Reset`);
+  const reset = createNewElement("a", "content", `${userName}Reset`);
   reset.innerText = "reset";
   reset.addEventListener("click", () => {
     score.innerText = "0";
@@ -84,9 +70,10 @@ function createDropdown(userName, scr) {
   dropdownContent.append(play, del, reset);
 }
 
-function createNewElement(tagName, className) {
+function createNewElement(tagName, className, id) {
   element = document.createElement(tagName);
   element.classList.add(className);
+  element.setAttribute("id", id);
   return element;
 }
 
@@ -94,7 +81,12 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function getSeconds() {
+  return Math.round(new Date().getTime() / 1000);
+}
+
 function checkMath(name) {
+  let secBefore = getSeconds();
   let score = 0;
   const operators = [
     {
@@ -111,11 +103,10 @@ function checkMath(name) {
     },
   ];
 
-  for (let i = 1; i <= 1; i++) {
-    const num1 = randomInt(1, 10);
-    const num2 = randomInt(1, 10);
+  for (let i = 1; i <= 5; i++) {
+    const num1 = randomInt(10, 99);
+    const num2 = randomInt(10, 99);
     const selectedOperator = randomInt(0, 1);
-
     const correctAnswer = operators[selectedOperator].method(num1, num2);
     userAnswer = prompt(
       `Please enter a correct answer in 2 seconds:\n ${num1} ${operators[selectedOperator]["sign"]} ${num2}`
@@ -125,12 +116,13 @@ function checkMath(name) {
     if (userAnswer === null) {
       return;
     }
-
+    let secAfter = getSeconds();
+    let timer = secAfter - secBefore;
     let userNum = parseInt(userAnswer);
-
-    if (userNum === correctAnswer) {
+    if (userNum === correctAnswer && timer <= 2) {
       score++;
     }
+    secBefore = secAfter;
   } // for
 
   const elementSpan = document.getElementById(`${name}Score`);
